d2f5bf3c947bb8420368a07601d8bb02f31fb479
张乃港 <zhangnaigang@xiaomings-MacBook-Air.local>
2021-06-27 19:00:33 +0800

    feat: add demo_add_data_to_mysql

---
+++ cost_of_context_switch_demo/cmake-build-debug/CMakeFiles/cost_of_context_switch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cost_of_context_switch.dir/main.cpp.o"
+  "cost_of_context_switch"
+  "cost_of_context_switch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cost_of_context_switch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
