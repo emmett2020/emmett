dadc81b5b491404d89ff627441da5780555b339a
xiaoming <59395781+Runner-2019@users.noreply.github.com>
2023-07-25 10:03:13 +0800

    feat: add author in main.cpp (#2)

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ using namespace std;  // NOLINT
 
 auto main() -> int {
   std::cout << "hello world" << std::endl;
+  std::cout << "you can find me by xiaomingZhang2020@outlook.com" << std::endl;
   return 0;
 }
