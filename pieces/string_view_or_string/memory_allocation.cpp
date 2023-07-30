cdb0d38f2b1d509681fce0a07f9527dd01755759
xiaomingzhang <xiaomingZhang2020@outlook.com>
2023-07-30 20:02:49 +0800

    add cstring for string_view_or_string and google bench mark

--- pieces/string_view_or_string/memory_allocation.cpp
+++ pieces/string_view_or_string/memory_allocation.cpp
@@ -5,8 +5,10 @@ void Func1(std::string_view str) {}
 
 void Func2(const std::string& str) {}
 
+void Func3(const char* str) {}
+
 static void UseStringView(benchmark::State& state) {
-  for (auto _ : state) {  // NOLINT
+  for (auto _ : state) {
     // Use string_view: no need to allocate memory buffer
     Func1("Hello world");
   }
@@ -15,7 +17,7 @@ static void UseStringView(benchmark::State& state) {
 BENCHMARK(UseStringView);  // NOLINT
 
 static void UseString(benchmark::State& state) {
-  for (auto _ : state) {  // NOLINT
+  for (auto _ : state) {
     // Use string: we must allocate memory to save string.
     // If the string is very long, this may be a huge time consuming
     Func2("Hello world");
@@ -25,7 +27,7 @@ static void UseString(benchmark::State& state) {
 BENCHMARK(UseString);  // NOLINT
 
 static void UseLongStringView(benchmark::State& state) {
-  for (auto _ : state) {  // NOLINT
+  for (auto _ : state) {
     // Use string_view: no need to allocate memory buffer
     Func1("Hello world Hello world Hello world Hello world Hello world ");
   }
@@ -34,7 +36,7 @@ static void UseLongStringView(benchmark::State& state) {
 BENCHMARK(UseLongStringView);  // NOLINT
 
 static void UseLongString(benchmark::State& state) {
-  for (auto _ : state) {  // NOLINT
+  for (auto _ : state) {
     // Use string: we must allocate memory to save string.
     // If the string is very long, this may be a huge time consuming
     Func2("Hello world Hello world Hello world Hello world Hello world ");
@@ -42,5 +44,21 @@ static void UseLongString(benchmark::State& state) {
 }
 
 BENCHMARK(UseLongString);  // NOLINT
+
+static void UseCStyleString(benchmark::State& state) {
+  for (auto _ : state) {
+    Func3("Hello world");
+  }
+}
+
+BENCHMARK(UseCStyleString);  // NOLINT
+
+static void UseLongCStyleString(benchmark::State& state) {
+  for (auto _ : state) {
+    Func3("Hello world Hello world Hello world Hello world Hello world ");
+  }
+}
+
+BENCHMARK(UseLongCStyleString);  // NOLINT
 
 BENCHMARK_MAIN();  // NOLINT
