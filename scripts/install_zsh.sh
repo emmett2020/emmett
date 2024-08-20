7eb4f5e584cac29007375be783cecffed535d2d4
xiaomingZhang2020 <xiaomingZhang2020@outlook.com>
2024-08-20 16:15:46 +0000

    fix zsh not effect error

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -13,11 +13,21 @@ function install_zsh() {
   echo "  oh-my-zsh will be installed into: ${OH_MY_ZSH_INSTALL_PATH}"
   echo -e "  ......\n\n"
 
+
+	local tmp="${HOME}/.tmp_install"
+  [[ -d "${tmp}" ]] && rm -r "${tmp}"
+  mkdir -p "${tmp}"
+
   sudo apt install zsh
   chsh -s /bin/zsh
   [[ -d "${OH_MY_ZSH_INSTALL_PATH}" ]] &&  rm -r "${OH_MY_ZSH_INSTALL_PATH}"
   [[ -f "${HOME}/.zshrc" ]] &&  rm -r "${HOME}/.zshrc"
-  sh -c "$(wget -O- ${OH_MY_ZSH_LINK})" --skip-chsh
+
+  wget "${OH_MY_ZSH_LINK}" -O "${tmp}/oh_my_zsh.sh"
+  bash "${tmp}/oh_my_zsh.sh" --unattended
+  rm -rf ${tmp}
+
+  echo "  successfully installed zsh and oh-my-zsh"
 }
 
 install_zsh
