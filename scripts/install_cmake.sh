6f7da4928b993831fb6da58ad98128ed66da8ffc
xiaomingZhang2020 <xiaomingZhang2020@outlook.com>
2024-08-20 05:46:48 +0000

    cmake scripts okay

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 set -e
-echo "Installing cmake"
 
 CMAKE_VERSION=3.29.2
 CMAKE_INSTALL_PATH="/usr/local"
-CMAKE_TEMP_PATH="/tmp/cmake_${CMAKE_VERSION}.sh"
+CMAKE_LINK="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh"
 
-wget "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh" -O ${CMAKE_TEMP_PATH}
-sudo bash ${CMAKE_TEMP_PATH} --skip-license --prefix="${CMAKE_INSTALL_PATH}" 
+function install_cmake() {
+  local tmp_path="${HOME}/tmp"
+  [[ -d "${tmp_path}" ]] && rm -rf "${tmp_path}"
+  mkdir -p "${tmp_path}"
+
+  echo "  Installing cmake ${CMAKE_VERSION} (needs sudo permission)"
+  echo "  Link: ${CMAKE_LINK}"
+  echo "  CMake will be installed into: ${CMAKE_INSTALL_PATH}"
+  echo -e "  ......\n\n"
+
+  wget ${CMAKE_LINK} -O "${tmp_path}/cmake_install.sh"
+  sudo bash "${tmp_path}/cmake_install.sh" --skip-license --prefix="${CMAKE_INSTALL_PATH}"
+
+  rm -rf "${tmp_path}"
+  cmake --version
+}
+
+install_cmake
