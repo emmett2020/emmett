5efdc5787070470460c422e73d676307dcc34a18
Emmett <emmettzhang2020@outlook.com>
2024-09-22 23:50:51 +0800

    update scripts and daily (#38)

--- scripts/install_lazygit.sh
+++ scripts/install_lazygit.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 set -e
 
-LAZYGIT_VERSION="0.43.1"
-LAZYGIT_LINK="https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-LAZYGIT_INSTALL_PATH="/usr/local/bin"
+lazygit_version="0.43.1"
+link_lazygit="https://github.com/jesseduffield/lazygit/releases/download/v${lazygit_version}/lazygit_${lazygit_version}_Linux_x86_64.tar.gz"
+dir_lazygit="/usr/local/bin"
 
-function get_lazygit_version() {
-	local installed_version=$(lazygit --version | awk -F, '{print $4}' | awk -F= '{print $2}')
-  echo ${installed_version}
+
+function print_hint() {
+  echo -e "  ......\n\n"
+  echo "  Lazygit download  link: ${link_lazygit}"
+  echo "  Lazygit installed path: ${dir_lazygit}"
 }
 
 function install_lazygit() {
-  echo "  Installing lazygit ${LAZYGIT_VERSION} (needs sudo permission) ......"
-  echo "  Lazygit Link: ${LAZYGIT_LINK}"
-  echo "  Lazygit install path: ${LAZYGIT_INSTALL_PATH}"
-  echo -e "  ......\n\n"
-
 	local tmp="${HOME}/.tmp_install"
 	local tmp_gz="${tmp}/lazygit.tar.gz"
   [[ -d "${tmp}" ]] && rm -r "${tmp}"
   mkdir -p ${tmp}
 
-  wget "${LAZYGIT_LINK}" -O "${tmp_gz}"
+  wget "${link_lazygit}" -O "${tmp_gz}"
   tar xf ${tmp_gz} -C ${tmp}
-  sudo install "${tmp}"/lazygit ${LAZYGIT_INSTALL_PATH}
+  sudo install "${tmp}"/lazygit ${dir_lazygit}
   rm -rf ${tmp}
 	lazygit --version
 }
 
 install_lazygit
+print_hint
--- scripts/install_ripgrep.sh
+++ scripts/install_ripgrep.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 set -e
 
-RIPGREP_VERSION="14.1.0"
-RIPGREP_LINK="https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}-1_amd64.deb"
+ripgrep_version="14.1.0"
+link_ripgrep="https://github.com/BurntSushi/ripgrep/releases/download/${ripgrep_version}/ripgrep_${ripgrep_version}-1_amd64.deb"
 
-# Get ripgrep version installed in this machine.
-function get_ripgrep_version() {
-	INSTALLED_VERSION=$(rg --version | grep ripgrep | awk '{print $2}')
-	return $?
+function print_hint() {
+  echo -e "  ......\n\n"
+  echo "  ripgrep download Link: ${link_ripgrep}"
 }
 
 function install_ripgrep() {
-  echo "  Installing ripgrep ${RIPGREP_VERSION} ......"
-  echo "  Link: ${RIPGREP_LINK}"
-  echo -e "  ......\n\n"
-
 	local tmp="${HOME}/.tmp_install"
   [[ -d "${tmp}" ]] && rm -r "${tmp}"
   mkdir -p "${tmp}"
 
-  wget ${RIPGREP_LINK} -O "${tmp}/ripgrep.deb"
+  wget ${link_ripgrep} -O "${tmp}/ripgrep.deb"
   sudo dpkg -i "${tmp}/ripgrep.deb"
   rm -rf ${tmp}
 	rg --version
 }
 
 install_ripgrep
+print_hint
