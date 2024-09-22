63154b65c419303dd5f457ed32a1233147e4d3bf
Emmett <emmettzhang2020@outlook.com>
2024-09-22 13:12:05 +0800

    Update zshrc to add some new plugins (#37)
    * add basic version
    
    * update basic zshrc
    
    * add chroma
    
    * works on macos
    
    * add pip
    
    * remove -x
    
    * add more hoint

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -1,33 +1,106 @@
 #!/bin/bash
 set -e
 
-# 1. Always install zsh default version.
+# 1. /path-to-emmett/configs/zshrc/daily must be existed.
+# 2. Could be used both on Ubuntu and MacOS
 
-CUR_SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
-OH_MY_ZSH_LINK=https://install.ohmyz.sh/
-OH_MY_ZSH_INSTALL_PATH="${HOME}/.oh-my-zsh"
+link_oh_my_zsh=https://install.ohmyz.sh/
+link_zsh_syntax_highlighting=https://github.com/zsh-users/zsh-syntax-highlighting
+link_zsh_auto_suggestions=https://github.com/zsh-users/zsh-autosuggestions
+link_powerlevel10k=https://gitee.com/romkatv/powerlevel10k.git
+link_eza="https://github.com/eza-community/eza/releases/download/v0.19.4/eza_x86_64-unknown-linux-gnu.tar.gz"
+link_chroma="https://github.com/alecthomas/chroma/releases/download/v2.14.0/chroma-2.14.0-linux-amd64.tar.gz"
 
-function install_zsh() {
-  echo "  Installing zsh (needs sudo permission)"
-  echo "  oh-my-zsh link: ${OH_MY_ZSH_LINK}"
-  echo "  oh-my-zsh will be installed into: ${OH_MY_ZSH_INSTALL_PATH}"
+cur_dir=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+omz_custom="${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"
+
+dir_oh_my_zsh="${HOME}/.oh-my-zsh"
+dir_zsh_syntax_highlighting="${omz_custom}/plugins/zsh-syntax-highlighting"
+dir_zsh_auto_suggesstions="${omz_custom}/plugins/zsh-autosuggestions"
+dir_powerlevel10k="${omz_custom}/themes/powerlevel10k"
+
+emmett_path="${cur_dir}/.."
+zshrc_path="${emmett_path}/configs/zshrc/daily"
+
+function get_system_info() {
+	if grep -q "Ubuntu" "/etc/os-release" ; then
+	  SYSTEM_TYPE="Ubuntu"
+	  SYSTEM_VERSION=$(cat /etc/os-release | grep VERSION_ID | awk -F\" '{print $2}')
+	  SYSTEM_ARCH=$(uname -m)
+	fi
+}
+
+
+function check_zshrc() {
+  if [[ ! -f "${zshrc_path}" ]]; then
+    echo "  Can't find .zshrc file in emmett2020/emmett repo. Path of .zshrc: ${zshrc_path}"
+    exit 1
+  fi
+  if [[ -f "${HOME}/.zshrc" ]]; then
+    echo "  Remove or save ${HOME}/.zshrc first."
+    exit 1
+  fi
+}
+
+function print_hint() {
   echo -e "  ......\n\n"
+  echo "  Installed zsh, config and plugins(needs sudo permission)"
+  echo "  oh-my-zsh download  link: ${link_oh_my_zsh}"
+  echo "  oh-my-zsh installed path: ${dir_oh_my_zsh}"
+  echo "  zsh-syntax-highlighting download  link: ${link_zsh_syntax_highlighting}"
+  echo "  zsh-syntax-highlighting installed path: ${dir_zsh_syntax_highlighting}"
+  echo "  zsh-autosuggestions download  link: ${link_zsh_auto_suggestions}"
+  echo "  zsh-autosuggestions installed path: ${dir_zsh_auto_suggesstions}"
+  echo "  powerlevel10k download  link: ${link_powerlevel10k}"
+  echo "  powerlevel10k installed path: ${dir_powerlevel10k}"
+  echo "  eza download link: ${link_eza}"
+  echo "  chroma download link: ${link_chroma}"
+  echo "  Successfully installed zsh, omz, themes, config and plugins."
+  echo "  Please use: source ~/.zshrc to apply newest config."
+}
 
-
+function install_omz() {
 	local tmp="${HOME}/.tmp_install"
   [[ -d "${tmp}" ]] && rm -r "${tmp}"
   mkdir -p "${tmp}"
 
-  sudo apt install zsh -y
-  chsh -s /bin/zsh
-  [[ -d "${OH_MY_ZSH_INSTALL_PATH}" ]] &&  rm -r "${OH_MY_ZSH_INSTALL_PATH}"
-  [[ -f "${HOME}/.zshrc" ]] &&  rm -r "${HOME}/.zshrc"
+  [[ -d "${dir_oh_my_zsh}" ]]               && rm -r "${dir_oh_my_zsh}"
+  [[ -d "${dir_powerlevel10k}" ]]           && rm -r "${dir_powerlevel10k}"
+  [[ -d "${dir_zsh_auto_suggesstions}" ]]   && rm -r "${dir_zsh_auto_suggesstions}"
+  [[ -d "${dir_zsh_syntax_highlighting}" ]] && rm -r "${dir_zsh_syntax_highlighting}"
 
-  wget "${OH_MY_ZSH_LINK}" -O "${tmp}/oh_my_zsh.sh"
+  source "${cur_dir}/detect_os.sh"
+  if [[ "${OS}" == "MacOS" ]]; then
+    brew install zsh chroma eva
+  elif [[ "${OS}" == "Ubuntu" ]]; then
+    sudo apt install -y zsh
+
+    wget "${link_eza}" -O "${tmp}/eva.tar.gz"
+    tar -xzf "${tmp}/eva.tar.gz" -C "${tmp}"
+    mv "${tmp}/eza" "/usr/local/bin"
+
+    wget "${link_chroma}" -O "${tmp}/chroma.tar.gz"
+    tar -xzf "${tmp}/chroma.tar.gz" -C "${tmp}"
+    mv "${tmp}/chroma" "/usr/local/bin"
+  else
+    exit 1
+  fi
+
+  # 2. Install oh my zsh
+  wget "${link_oh_my_zsh}" -O "${tmp}/oh_my_zsh.sh"
   bash "${tmp}/oh_my_zsh.sh" --unattended
-  rm -rf ${tmp}
 
-  echo "  successfully installed zsh and oh-my-zsh"
+  # 3. Install plugins and themes
+  git clone "${link_zsh_syntax_highlighting}" "${dir_zsh_syntax_highlighting}"
+  git clone "${link_zsh_auto_suggestions}"    "${dir_zsh_auto_suggesstions}"
+  git clone --depth=1 "${link_powerlevel10k}" "${dir_powerlevel10k}"
+
+  # 6. Copy zshrc from emmett repo
+  cp "${zshrc_path}" ~/.zshrc
+
+  rm -rf ${tmp}
 }
 
-install_zsh
+check_zshrc
+install_omz
+print_hint
