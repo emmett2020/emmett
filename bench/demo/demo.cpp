e159e21033c378157a73d7c69cb31a84cbd97cb6
emmett <emmettzhang2020@outlook.com>
2024-10-23 13:05:36 +0000

    remove useless comments

--- bench/demo/demo.cpp
+++ bench/demo/demo.cpp
@@ -2,23 +2,6 @@
 
 #include <benchmark/benchmark.h>
 
-// Provide a way to tell if a character is
-//  a digit
-//  or a alpha
-//  or in a collection of digit, alpha and other special characters.
-
-// Result:
-// The fastest way is to use operator directly. But it make the code not easy to
-// read. Especially when the expression becomes complicated.
-
-// The awesome way is to lookup the table and elide the function call use always
-// inline attribute.
-
-//  The slowest way is to use std::is_xxx. One shouldn't use it in a base
-//  library.
-
-// Real output with gcc:
-
 static void Func1(benchmark::State &state) {
   char c = '8';
   for (auto _ : state) {
