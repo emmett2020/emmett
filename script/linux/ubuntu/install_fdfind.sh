1102d6b76c8b4cffb101b7dc459bbff9511ce6ed
root <emmettzhang2020@outlook.com>
2025-01-16 04:31:57 +0000

    install fdfind and remove codelldb&cppdbg

---
+++ script/linux/ubuntu/install_fdfind.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+: << 'COMMENT'
+|------------------------------|------------------------------|
+|         🎃 item              |        👇 explanation        |
+|------------------------------|------------------------------|
+|    needs root permission?    |              Yes             |
+|------------------------------|------------------------------|
+|          dependencies        |              No              |
+|------------------------------|------------------------------|
+COMMENT
+
+set -euo pipefail
+arch=$(uname -m)
+[[ "${arch}" == "x86_64" ]]  && arch="amd64"
+[[ "${arch}" == "aarch64" ]] && arch="arm64"
+
+fdfind_version="10.1.0"
+fdfind_link="https://github.com/sharkdp/fd/releases/download/v${fdfind_version}/fd_${fdfind_version}_${arch}.deb"
+
+temp_dir=$(mktemp -d)
+trap "rm -rf ${temp_dir}" EXIT
+
+wget ${fdfind_link} -O "${temp_dir}/fdfind.deb"
+sudo dpkg -i "${temp_dir}/fdfind.deb"
+fd --version
+
