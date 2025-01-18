ff722879a129c4a6169a67a2d770a479acbc71d0
emmett <emmettzhang2020@outlook.com>
2025-01-18 16:13:04 +0000

    refine more

--- script/linux/ubuntu/arm/build_nvim.sh
+++ script/linux/ubuntu/arm/build_nvim.sh
@@ -16,12 +16,14 @@ temp_dir=$(mktemp -d)
 trap "rm -rf ${temp_dir}" EXIT
 pushd ${temp_dir} &> /dev/null
 
-version="0.10.3"
 git clone --recursive https://github.com/neovim/neovim.git
+cd neovim
+
+version="0.10.3"
 git checkout "v${version}"
 
-make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/full/path/
-make install
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/usr/local
+sudo make install
 
 popd &> /dev/null
 
--- script/linux/ubuntu/arm/v2404/build_on_2404.sh
+++ script/linux/ubuntu/arm/v2404/build_on_2404.sh
@@ -1,8 +1,5 @@
 #/bin/bash
-
-mkdir build
 cmake -S . -B build
 cmake --build build -j16
 
-# package
 
