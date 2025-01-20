88ab339cdf2309346577725267182b13e400f6dc
emmett <emmettzhang2020@outlook.com>
2025-01-20 16:26:36 +0000

    feat: add cland compilation

--- script/linux/ubuntu/build_clangd.sh
+++ script/linux/ubuntu/build_clangd.sh
@@ -10,15 +10,24 @@
 COMMENT
 set -euo pipefail
 
-arch=$(uname -m)
-
-cmake_version=3.31.4
-cmake_link="https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}-linux-${arch}.sh"
-cmake_dir="/usr/local"
-
 temp_dir=$(mktemp -d)
-trap "rm -rf ${temp_dir}" EXIT
+# trap "rm -rf ${temp_dir}" EXIT
 
-wget ${cmake_link} -O "${temp_dir}/cmake_install.sh"
-sudo bash "${temp_dir}/cmake_install.sh" --skip-license --prefix="${cmake_dir}"
-cmake --version
+clangd_version=llvmorg-19.1.7
+clangd_link="https://github.com/llvm/llvm-project.git"
+clangd_dir="/usr/local"
+
+pushd ${temp_dir} &> /dev/null
+sudo apt install ninja-build
+
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+git checkout "${clangd_version}"
+git submodule update --init --recursive
+mkdir build && cd build
+cmake ../llvm/ -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -GNinja
+cmake --build . --target clangd -j`nproc`
+
+popd &> /dev/null
+
+clangd --version
