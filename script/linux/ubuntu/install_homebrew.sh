f1cdab01bce4465f317b5a926ab0533c621e5166
root <emmettzhang2020@outlook.com>
2025-01-23 05:06:31 +0000

    add configfile and homebrew sh

--- script/linux/ubuntu/install_homebrew.sh
+++ script/linux/ubuntu/install_homebrew.sh
@@ -11,6 +11,9 @@ COMMENT
 set -euo pipefail
 
 NONINTERACTIVE=1 CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> ${HOME}/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ${HOME}/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew --version
 brew update
 
