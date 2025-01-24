8e1ede18be87706d2caa33b5280980bdf8262f71
root <emmettzhang2020@outlook.com>
2025-01-24 06:15:18 +0000

    add check

--- script/linux/ubuntu/x64/manual_distribution/with_compiler/install.sh
+++ script/linux/ubuntu/x64/manual_distribution/with_compiler/install.sh
@@ -25,7 +25,7 @@ LIB_INSTALL_PATH="/usr/local/lib/${BINARY_NAME}"
 BIN_INSTALL_PATH="/usr/local/bin/"
 
 # Refresh libraries
-if [[ -d ""${LIB_INSTALL_PATH}"" ]]; then
+if [[ -d "${LIB_INSTALL_PATH}" ]]; then
   echo "Remove old ${BINARY_NAME} libraries"
   sudo rm -rf "${LIB_INSTALL_PATH}"
 fi
@@ -41,4 +41,4 @@ sudo mv "${CUR_SCRIPT_DIR}"/bin/* ${BIN_INSTALL_PATH}
 
 echo "Successfully installed  ${BINARY_NAME}"
 echo "Binaries install path:  ${BIN_INSTALL_PATH}"
-echo "Libraries install path: "${LIB_INSTALL_PATH}""
+echo "Libraries install path: ${LIB_INSTALL_PATH}"
--- script/linux/ubuntu/x64/manual_distribution/with_compiler/uninstall.sh
+++ script/linux/ubuntu/x64/manual_distribution/with_compiler/uninstall.sh
@@ -20,7 +20,7 @@ LIB_INSTALL_PATH="/usr/local/lib/${BINARY_NAME}/"
 BIN_INSTALL_PATH="/usr/local/bin/"
 
 # Refresh libraries
-if [[ -d ""${LIB_INSTALL_PATH}"" ]]; then
+if [[ -d "${LIB_INSTALL_PATH}" ]]; then
   echo "Remove old ${BINARY_NAME} libraries"
   sudo rm -rf "${LIB_INSTALL_PATH}"
 fi
