eebb12ec7ea28e9a8d5c5f111f386e7bf1460d7a
emmett <emmettzhang2020@outlook.com>
2025-06-08 13:10:39 +0000

    try

--- script/linux/ubuntu/install_gcc.sh
+++ script/linux/ubuntu/install_gcc.sh
@@ -17,11 +17,18 @@ CUR_SCRIPT_DIR=$(
 echo "Add universe warehose firstly"
 bash "${CUR_SCRIPT_DIR}"/add_universe_warehose.sh
 
+# Only support Ubuntu 24.04+ now.
+. /etc/os-release
+if [[ "${VERSION_ID%%.*}" -lt 24 ]]; then
+  echo "Unsupported OS system version."
+  exit 0
+fi
+
 echo "Install gcc-14"
 sudo apt install -y gcc-14 g++-14 --fix-missing
 gcc-14 --version
 g++-14 --version
 
 echo "Change it to default version"
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 14
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 14
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 14
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 14
