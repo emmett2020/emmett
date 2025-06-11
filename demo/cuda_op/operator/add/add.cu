4add1f6b31bb1eb3be94a336997665e5cf66fc72
emmett <emmettzhang2020@outlook.com>
2025-06-11 12:38:29 +0000

    fix some

--- demo/cuda_op/operator/add/add.cu
+++ demo/cuda_op/operator/add/add.cu
@@ -7,13 +7,17 @@
 
 namespace cuda_op {
   template <class DataType>
-  __global__ void add(const DataType* __restrict__ x1_buf, const DataType* __restrict__ x2_buf, int N, DataType* __restrict__ y_buf) {
+  __global__ void
+  add(const DataType* __restrict__ x1_buf,
+      const DataType* __restrict__ x2_buf,
+      int N,
+      DataType* __restrict__ y_buf) {
     const auto stride = gridDim.x * blockDim.x;
     for (std::size_t i = (blockDim.x * blockIdx.x) + threadIdx.x; i < N; i += stride) {
       DataType x1 = x1_buf[i];
       DataType x2 = x2_buf[i];
-      DataType y = x1 + x2;
-      y_buf[i]   = y; 
+      DataType y  = x1 + x2;
+      y_buf[i]    = y;
     }
   }
 
@@ -26,10 +30,10 @@ namespace cuda_op {
 
   // TODO: bind pybind11 type to launch_relu
 
-void add_op_add(pybind11::module& m) {
-  m.def("add", &launch_add<float>, "A function that add two tensors");
-}
+  void add_op_add(pybind11::module& m) {
+    m.def("add", &launch_add<float>, "A function that add two tensors");
+  }
 
-} // namespace
+} // namespace cuda_op
 
 
