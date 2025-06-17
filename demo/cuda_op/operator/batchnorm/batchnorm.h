b36b7fb592a9bca657a6bf99daefc96641d4bfae
emmett <emmettzhang2020@outlook.com>
2025-06-17 05:00:05 +0000

    batchnorm passed

--- demo/cuda_op/operator/batchnorm/batchnorm.h
+++ demo/cuda_op/operator/batchnorm/batchnorm.h
@@ -5,10 +5,10 @@
 namespace cuda_op {
   torch::Tensor torch_batch_norm(
     const torch::Tensor& input,
-    const torch::Tensor& gamma,
-    const torch::Tensor& beta,
     torch::Tensor& running_mean,
     torch::Tensor& running_var,
+    const torch::Tensor& gamma,
+    const torch::Tensor& beta,
     float epsilon,
     float momentum,
     bool training);
--- demo/cuda_op/test/test_batch_norm.py
+++ demo/cuda_op/test/test_batch_norm.py
@@ -4,35 +4,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def test_basic():
+def test_training():
     N, C, H, W = 2, 4, 8, 8
     epsilon = 1e-5
     momentum = 0.9
 
-    input_tensor = torch.randn(N, C, H, W)
+    input = torch.randn(N, C, H, W)
     gamma = torch.randn(C)
     beta = torch.randn(C)
     running_mean = torch.zeros(C)
     running_var = torch.ones(C)
-
-    pt_input = input_tensor.clone().detach().requires_grad_(False)
-    pt_gamma = gamma.clone().detach()
-    pt_beta = beta.clone().detach()
-    pt_running_mean = running_mean.clone().detach()
-    pt_running_var = running_var.clone().detach()
+    cuda_running_mean = running_mean.to("cuda").clone().detach()
+    cuda_running_var = running_var.to("cuda").clone().detach()
 
     print("=== training mode ===")
-    mean = pt_input.mean(dim=(0, 2, 3))
-    var = pt_input.var(dim=(0, 2, 3), unbiased=False)
-    golden = F.batch_norm(pt_input,
-                          mean,
-                          var,
-                          pt_gamma,
-                          pt_beta,
+    mean = input.mean(dim=(0, 2, 3))
+    var = input.var(dim=(0, 2, 3), unbiased=False)
+    golden = F.batch_norm(input, running_mean, running_var, gamma, beta,
                           training=True,
                           eps=epsilon)
 
-    actual = torch.empty_like(input_tensor, device="cuda")
-    cuda_op.batch_norm(pt_input, mean, var, pt_gamma, pt_beta, epsilon,
-                       momentum, True)
+    input = input.to("cuda")
+    gamma = gamma.to("cuda")
+    beta = beta.to("cuda")
+    running_mean = cuda_running_mean
+    running_var = cuda_running_var
+    actual = cuda_op.batch_norm(input, running_mean, running_var, gamma, beta, epsilon,
+                                momentum, True)
+    actual = actual.to("cpu")
     torch.testing.assert_close(golden, actual, atol=1e-5, rtol=1e-5)
