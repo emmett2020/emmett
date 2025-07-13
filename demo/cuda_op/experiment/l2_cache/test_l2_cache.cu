5e4fc25bc447562679e59cba3a472f11ab0b9eb0
emmett <emmettzhang2020@outlook.com>
2025-07-13 14:34:19 +0000

    add cpu golden

--- demo/cuda_op/experiment/l2_cache/test_l2_cache.cu
+++ demo/cuda_op/experiment/l2_cache/test_l2_cache.cu
@@ -74,13 +74,11 @@ namespace {
     fill_random_data_kernel<<<grid_size, block_size>>>(d_state, d_data, num_elements, seed);
   }
 
-  void print_device_buffer(const float* d_data, int H, int W, std::string_view title = "") {
+  void print_host_buffer(const float* h_data, int H, int W, std::string_view title = "") {
     throw_if(H <= 0, "H must be greater than 0");
     throw_if(W <= 0, "W must be greater than 0");
     int num_elements = H * W;
     size_t byte_size = num_elements * sizeof(float);
-    std::vector<float> h_data(num_elements);
-    cuda_check(cudaMemcpy(h_data.data(), d_data, byte_size, cudaMemcpyDeviceToHost));
     if (!title.empty()) {
       std::cout << title << "\n";
     }
@@ -90,6 +88,28 @@ namespace {
         std::cout << h_data[(h * W) + w] << ", ";
       }
       std::cout << h_data[(h * W) + W - 1] << "\n";
+    }
+  }
+
+  void print_device_buffer(const float* d_data, int H, int W, std::string_view title = "") {
+    throw_if(H <= 0, "H must be greater than 0");
+    throw_if(W <= 0, "W must be greater than 0");
+    int num_elements = H * W;
+    size_t byte_size = num_elements * sizeof(float);
+    std::vector<float> h_data(num_elements);
+    cuda_check(cudaMemcpy(h_data.data(), d_data, byte_size, cudaMemcpyDeviceToHost));
+    print_host_buffer(h_data.data(), H, W, title);
+  }
+
+  void cpu_mma(int M, int N, int K, const float* a_ptr, const float* b_ptr, float* c_ptr) {
+    for (int m = 0; m < M; ++m) {
+      for (int n = 0; n < N; ++n) {
+        float sum = 0;
+        for (int k = 0; k < K; ++k) {
+          sum += a_ptr[(m * K) + k] * b_ptr[(n * K) + k];
+        }
+        c_ptr[(m * N) + n] = sum;
+      }
     }
   }
 } // namespace
@@ -113,69 +133,47 @@ auto main() noexcept(false) -> int {
     << cache_line_elements_cnt
     << "\n";
 
-  const int K = cache_line_elements_cnt;
+  // const int K = cache_line_elements_cnt;
+  const int K = 3;
   // const int M = cache_elements_cnt / K / 2;
   // const int N = cache_elements_cnt / K / 2;
 
-  const int M = 128;
-  const int N = 8;
+  const int M = 2;
+  const int N = 2;
 
-  const int num_tiles_m = 128;
-  const int num_tiles_n = 8;
-  const uint32_t tile_m = M / num_tiles_m;
-  const uint32_t tile_n = N / num_tiles_n;
-  auto grid_dim         = dim3{num_tiles_m, num_tiles_n};
-  auto block_dim        = dim3{tile_m};
+  // const int num_tiles_m = 128;
+  // const int num_tiles_n = 8;
+  // const uint32_t tile_m = M / num_tiles_m;
+  // const uint32_t tile_n = N / num_tiles_n;
+  // auto grid_dim         = dim3{num_tiles_m, num_tiles_n};
+  // auto block_dim        = dim3{tile_m};
 
   std::cout << "M=" << M << ", K=" << K << ", N=" << N << "\n";
-  std::cout << "tile_m=" << tile_m << ", tile_n=" << tile_n << "\n";
+  // std::cout << "tile_m=" << tile_m << ", tile_n=" << tile_n << "\n";
 
-  const size_t a_size = static_cast<uint64_t>(M * K) * sizeof(float);
-  const size_t b_size = static_cast<uint64_t>(K * N) * sizeof(float);
-  const size_t c_size = static_cast<uint64_t>(M * N) * sizeof(float);
-  float* a_ptr        = nullptr;
-  float* b_ptr        = nullptr;
-  float* c_ptr        = nullptr;
+  const auto a_num_eles = static_cast<int>(M * K);
+  const auto b_num_eles = static_cast<int>(K * N);
+  const auto c_num_eles = static_cast<int>(M * N);
+  const size_t a_size   = static_cast<uint64_t>(M * K) * sizeof(float);
+  const size_t b_size   = static_cast<uint64_t>(K * N) * sizeof(float);
+  const size_t c_size   = static_cast<uint64_t>(M * N) * sizeof(float);
+  float* a_ptr          = nullptr;
+  float* b_ptr          = nullptr;
+  float* c_ptr          = nullptr;
   cuda_check(cudaMalloc(&a_ptr, a_size));
   cuda_check(cudaMalloc(&b_ptr, b_size));
   cuda_check(cudaMalloc(&c_ptr, c_size));
 
   std::random_device rd{};
-  uint64_t seed = rd();
   fill_random_data(a_ptr, M * K, rd());
-  fill_random_data(b_ptr, K * N, rd());
+  fill_random_data(b_ptr, N * K, rd());
 
-
-  // cublasHandle_t handle = nullptr;
-  // cublas_check(cublasCreate(&handle));
-  //
-  // cublas_check(cublasSetMathMode(handle, CUBLAS_TENSOR_OP_MATH));
-  // float alpha = 1.0F;
-  // float beta  = 0.0F;
-
-  // 执行 FP16 矩阵乘法（内部自动调用 Tensor Core）
-  // cublas_check(cublasGemmEx(
-  //   handle,
-  //   CUBLAS_OP_N,
-  //   CUBLAS_OP_N,        // A 和 B 是否转置
-  //   M,
-  //   N,
-  //   K,                  // 矩阵维度
-  //   &alpha,             // alpha
-  //   A,
-  //   CUDA_R_16F,
-  //   M,                  // A 矩阵（FP16）
-  //   B,
-  //   CUDA_R_16F,
-  //   K,                  // B 矩阵（FP16）
-  //   &beta,              // beta
-  //   C,
-  //   CUDA_R_32F,
-  //   M,                  // C 矩阵（FP32）
-  //   CUDA_R_32F,         // 计算精度（FP32）
-  //   CUBLAS_GEMM_DEFAULT // 算法（自动选择 Tensor Core）
-  //   ));
-
+  std::vector<float> a_cpu(static_cast<size_t>(M * K), 0);
+  std::vector<float> b_cpu(static_cast<size_t>(N * K), 0);
+  std::vector<float> c_cpu(static_cast<size_t>(M * N), 0);
+  cuda_check(cudaMemcpy(a_cpu.data(), a_ptr, a_size, cudaMemcpyKind::cudaMemcpyDeviceToHost));
+  cuda_check(cudaMemcpy(b_cpu.data(), b_ptr, b_size, cudaMemcpyKind::cudaMemcpyDeviceToHost));
+  cpu_mma(M, N, K, a_cpu.data(), b_cpu.data(), c_cpu.data());
 
   // matmul_l2_cache_hit_rate_100<<<grid_dim, block_dim>>>(
   //   a_ptr,
@@ -188,8 +186,10 @@ auto main() noexcept(false) -> int {
   //   c_ptr);
 
   print_device_buffer(a_ptr, M, K, "a_ptr");
-  print_device_buffer(b_ptr, K, N, "b_ptr");
-  print_device_buffer(c_ptr, M, N, "c_ptr");
+  print_device_buffer(b_ptr, N, K, "b_ptr");
+  print_host_buffer(c_cpu.data(), M, N, "c_cpu_ptr");
+  print_device_buffer(c_ptr, M, N, "c_dev_ptr");
+
   cuda_check(cudaFree(a_ptr));
   cuda_check(cudaFree(b_ptr));
   cuda_check(cudaFree(c_ptr));
