9e78ad6e7c4ada7dac171c737888e62ecda5ad9c
emmett <emmettzhang2020@outlook.com>
2025-08-05 09:23:21 +0000

    add python3 attn

---
+++ demo/cuda_op/experiment/python3_attn/attn.py
@@ -0,0 +1,13 @@
+import torch
+
+
+def attention(query, key, value, mask=None, dropout=None):
+    "Compute 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)  # mask步骤，用 -1e9 代表负无穷
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
