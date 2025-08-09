789e67df2f3e80566bb6f41edcdf2437dd27c726
emmett <emmettzhang2020@outlook.com>
2025-08-09 09:20:36 +0000

    fix

--- demo/cuda_op/experiment/memory_access_vectorized/test_vectorized.cu
+++ demo/cuda_op/experiment/memory_access_vectorized/test_vectorized.cu
@@ -3,22 +3,38 @@
 #include <iostream>
 
 namespace {
-  __global__ void per_thread_1_element(float* ptr, unsigned N) {
-    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < N) {
+
+  /// NOTE:
+  /// To simplify, N should be divisible by 4
+
+  __global__ void per_thread_1_element_grid_stride(float* ptr, unsigned N) {
+    size_t idx    = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t stride = gridDim.x * blockDim.x;
+    for (size_t i = idx; i < N; i += stride) {
       ptr[idx] = static_cast<float>(idx);
     }
   }
 
-  // To simplify, N should be divisible by 4
-  __global__ void per_thread_4_elements(float* ptr, unsigned N) {
-    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx * 4 >= N) {
-      return;
+  __global__ void per_thread_4_elements_grid_stride(float* ptr, unsigned N) {
+    size_t idx    = blockIdx.x * blockDim.x + threadIdx.x * 4;
+    size_t stride = gridDim.x * blockDim.x * 4;
+    for (size_t i = idx; i < N; i += stride) {
+#pragma unroll
+      for (int j = 0; j < 4; ++j) {
+        ptr[idx + j] = static_cast<float>(idx + j);
+      }
     }
-    float4 data = make_float4(0, 1, 2, 3);
-    auto* ptr4  = reinterpret_cast<float4*>(ptr);
-    ptr4[idx]   = data;
+  }
+
+  __global__ void per_thread_4_elements_vectorized(float* ptr, unsigned N) {
+    size_t idx        = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t global_idx = blockIdx.x * blockDim.x + threadIdx.x * 4;
+    size_t stride     = gridDim.x * blockDim.x * 4;
+    for (size_t i = global_idx; i < N; i += stride) {
+      auto* ptr4 = reinterpret_cast<float4*>(ptr);
+      auto data = make_float4(global_idx, global_idx + 1, global_idx + 2, global_idx + 3); // NOLINT
+      ptr4[idx] = data;
+    }
   }
 
 
@@ -30,14 +46,21 @@ int main() {
   cudaMalloc(&ptr, N * 4);
 
   {
-    size_t vectorized_n = (N + 4 - 1) / 4;
-    size_t grid_size    = (vectorized_n + 127) / 128;
-    per_thread_4_elements<<<grid_size, 128>>>(ptr, N);
+    size_t grid_size = (N + 127) / 128;
+    per_thread_1_element_grid_stride<<<grid_size, 128>>>(ptr, N);
   }
 
   {
-    size_t grid_size = (N + 127) / 128;
-    per_thread_1_element<<<grid_size, 128>>>(ptr, N);
+    size_t vectorized_n = N / 4;
+    size_t grid_size    = (vectorized_n + 127) / 128;
+    per_thread_4_elements_vectorized<<<grid_size, 128>>>(ptr, N);
+  }
+
+
+  {
+    size_t vectorized_n = N / 4;
+    size_t grid_size    = (vectorized_n + 127) / 128;
+    per_thread_4_elements_grid_stride<<<grid_size, 128>>>(ptr, N);
   }
 
   cudaDeviceSynchronize();
