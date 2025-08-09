0aafdbce8df9fd927fb9cc5e6f1861207d24dccd
emmett <emmettzhang2020@outlook.com>
2025-08-09 05:36:38 +0000

    start to refine formula

--- demo/cuda_op/test/test_layer_norm.py
+++ demo/cuda_op/test/test_layer_norm.py
@@ -1,10 +1,9 @@
 import torch
 import cuda_op
 
-shape = [1, 2]
-
 
 def test_basic():
+    shape = [1, 2]
     x = torch.randn(shape, device='cpu')
     gamma = torch.ones(shape[1], device='cpu')
     beta = torch.zeros(shape[1], device='cpu')
@@ -19,3 +18,24 @@ def test_basic():
                                actual.cpu(),
                                atol=1e-5,
                                rtol=1e-5)
+
+
+def test_perf():
+    N = 16
+    C = 128
+    H = 256
+    W = 256
+    eps = 1e-5
+    shape = [N, C, H, W]
+    normalized_shape = (C, H, W)
+
+    x = torch.randn(shape, device='cuda')
+    gamma = torch.randn(normalized_shape, device='cuda')
+    beta = torch.randn(normalized_shape, device='cuda')
+    golden = torch.nn.functional.layer_norm(x,
+                                            normalized_shape,
+                                            weight=gamma,
+                                            bias=beta,
+                                            eps=eps)
+    # actual = cuda_op.layer_norm(x, gamma, beta, eps)
+    # torch.testing.assert_close(golden, actual, atol=1e-5, rtol=1.3e-6)
