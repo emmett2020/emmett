c2106bd11c13ff26b81dc49f03ebe5200d8fe95c
emmett <emmettzhang2020@outlook.com>
2025-08-10 03:48:39 +0000

    add bypass l1 config

--- demo/cuda_op/experiment/bypass_l1/test_l1_cache.cu
+++ demo/cuda_op/experiment/bypass_l1/test_l1_cache.cu
@@ -3,6 +3,21 @@
 #include <cstdint>
 
 namespace {
+  __global__ void transpose(const float* A, const int N, float* B) {
+    const unsigned nx = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned ny = blockIdx.y * blockDim.y + threadIdx.y;
+    if (nx < N && ny < N) {
+      B[ny * N + nx] = A[nx * N + ny];
+    }
+  }
+
+  __global__ void transpose_bypass(const float* A, const int N, float* B) {
+    const unsigned nx = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned ny = blockIdx.y * blockDim.y + threadIdx.y;
+    if (nx < N && ny < N) {
+      B[ny * N + nx] = __ldg(&A[nx * N + ny]);
+    }
+  }
 
   __global__ void normal(const float* input, unsigned N, float* output) {
     unsigned idx = threadIdx.x + blockIdx.x * blockDim.x;
@@ -32,7 +47,16 @@ auto main() noexcept(false) -> int {
   cudaMalloc(&a_ptr, size);
   cudaMalloc(&b_ptr, size);
 
-  const unsigned grid_size = (N + 127) / 128;
+  cudaFuncSetCacheConfig(normal, cudaFuncCache::cudaFuncCachePreferL1);
+  cudaFuncSetCacheConfig(bypass, cudaFuncCache::cudaFuncCachePreferL1);
+
+  int grid_size = N / 128;
   normal<<<grid_size, 128>>>(a_ptr, N, b_ptr);
   bypass<<<grid_size, 128>>>(a_ptr, N, b_ptr);
+
+
+  // const dim3 grid_size = {N / 32, N / 32};
+  // const dim3 blk_size  = {32, 32};
+  // transpose<<<grid_size, blk_size>>>(a_ptr, N, b_ptr);
+  // transpose_bypass<<<grid_size, blk_size>>>(a_ptr, N, b_ptr);
 }
