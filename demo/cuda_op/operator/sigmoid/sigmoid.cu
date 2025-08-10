b0ea6f36fd924129aab45fc66946bee87a6d3805
emmett <emmettzhang2020@outlook.com>
2025-08-10 14:34:45 +0000

    sigmoid done

--- demo/cuda_op/operator/sigmoid/sigmoid.cu
+++ demo/cuda_op/operator/sigmoid/sigmoid.cu
@@ -7,20 +7,44 @@
 
 namespace cuda_op {
   template <typename T>
-  __global__ void sigmoid(const T* input, int N, T* output) {
+  __global__ void sigmoid(const T* __restrict__ input_ptr, int N, T* __restrict__ output_ptr) {
     const int idx    = blockIdx.x * blockDim.x + threadIdx.x;
     const int stride = gridDim.x * blockDim.x;
-    for (int i = idx; i < N; ++i) {
-      auto v    = -1 * input[i];
-      auto o    = 1 / (1 + exp(v));
-      output[i] = o;
+    for (int i = idx; i < N; i += stride) {
+      auto x        = input_ptr[i];
+      auto o        = 1 / (1 + exp(-x));
+      output_ptr[i] = o;
     }
+  }
+
+  __global__ void
+  sigmoid_vectorized(const float* __restrict__ input_ptr, int N, float* __restrict__ output_ptr) {
+    const float4* input_ptr4 = reinterpret_cast<const float4*>(input_ptr);
+    float4* output_ptr4      = reinterpret_cast< float4*>(output_ptr);
+
+    const int idx    = blockIdx.x * blockDim.x + threadIdx.x;
+    const int stride = gridDim.x * blockDim.x;
+    for (int i = idx; i < N / 4; i += stride) {
+      float4 x       = input_ptr4[i];
+      auto o0        = 1 / (1 + exp(-x.x));
+      auto o1        = 1 / (1 + exp(-x.y));
+      auto o2        = 1 / (1 + exp(-x.z));
+      auto o3        = 1 / (1 + exp(-x.w));
+      output_ptr4[i] = make_float4(o0, o1, o2, o3);
+    }
+  }
+
+  cudaError_t launch_sigmoid_vectorized(const float* input, int N, float* output) {
+    const int blk_size  = 256;
+    const int grid_size = (N + blk_size - 1) / blk_size / 4;
+    sigmoid_vectorized<<<grid_size, blk_size>>>(input, N, output);
+    return cudaGetLastError();
   }
 
   template <typename T>
   cudaError_t launch_sigmoid(const T* input, int N, T* output) {
-    const int blk_size  = 128;
-    const int grid_size = (N + blk_size - 1) / blk_size * blk_size;
+    const int blk_size  = 256;
+    const int grid_size = (N + blk_size - 1) / blk_size;
     sigmoid<<<grid_size, blk_size>>>(input, N, output);
     return cudaGetLastError();
   }
@@ -32,7 +56,8 @@ namespace cuda_op {
     if (input.scalar_type() == torch::kFloat32) {
       auto* input_ptr  = input.data_ptr<float>();
       auto* output_ptr = output.data_ptr<float>();
-      cuda_check(launch_sigmoid<float>(input_ptr, N, output_ptr));
+      // cuda_check(launch_sigmoid<float>(input_ptr, N, output_ptr));
+      cuda_check(launch_sigmoid_vectorized(input_ptr, N, output_ptr));
     } else {
       std::string type_str = torch::toString(input.scalar_type());
       TORCH_CHECK(false, "Unsupported data type of input ", type_str);
