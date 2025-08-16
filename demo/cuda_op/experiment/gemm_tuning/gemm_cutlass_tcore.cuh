2e041c0d8a3b18550c2b4c209cc2138efe510d52
emmett <emmettzhang2020@outlook.com>
2025-08-16 15:28:51 +0000

    cutlass use tcore but mismatched

--- demo/cuda_op/experiment/gemm_tuning/gemm_cutlass_tcore.cuh
+++ demo/cuda_op/experiment/gemm_tuning/gemm_cutlass_tcore.cuh
@@ -1,25 +1,40 @@
+#include "cutlass/arch/mma.h"
+#include "cutlass/half.h"
 #include <cutlass/gemm/device/gemm.h>
+#include <cutlass/cutlass.h>
 
 namespace {
+
+
   cudaError_t CutlassGemmAmpereTcore(const half* A, const half* B, int M, int N, int K, half* C) {
-    using RowMajor = cutlass::layout::RowMajor;
-    using CutlassGemm =
-      cutlass::gemm::device::Gemm< half, RowMajor, half, RowMajor, half, RowMajor >;
+    using RowMajor    = cutlass::layout::RowMajor;
+    using CutlassGemm = cutlass::gemm::device::Gemm<
+      cutlass::half_t,
+      RowMajor,
+      cutlass::half_t,
+      RowMajor,
+      cutlass::half_t,
+      RowMajor,
+      cutlass::half_t,
+      cutlass::arch::OpClassTensorOp,
+      cutlass::arch::Sm80>;
+
     CutlassGemm gemm_operator{};
 
     int lda = M;
     int ldb = K;
     int ldc = M;
 
-    float alpha = 1.0F;
-    float beta  = 0.0F;
+    cutlass::half_t alpha(1.0F);
+    cutlass::half_t beta(0.0F);
 
+    // WARN: Mismatched, however, we don't care it's output now.
     CutlassGemm::Arguments args{
       {M, N, K},
-      {A, lda},
-      {B, ldb},
-      {C, ldc},
-      {C, ldc},
+      {reinterpret_cast<const cutlass::half_t*>(A), lda},
+      {reinterpret_cast<const cutlass::half_t*>(B), ldb},
+      {reinterpret_cast<cutlass::half_t*>(C), ldc},
+      {reinterpret_cast<cutlass::half_t*>(C), ldc},
       {alpha, beta}
     };
 
